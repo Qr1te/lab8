e80deaea97a7ed3ba73f95d6c69f2a45a5246b7a
Qr1te <roman.dashkevich3@gmail.com>
2025-02-28 00:39:47 +0300

    first commit

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 int main(int n_arg, char *arg[]) {
 
     char *filename = "file", *filename1 = "file1";
+    nameFile( &filename, &filename1, n_arg, arg);
     srand(time(0));
     int ans;
     do {
@@ -14,14 +15,10 @@ int main(int n_arg, char *arg[]) {
         setNumber(&ans);
         fflush(stdin);
         switch(ans) {
-            case 1: nameFile( &filename, &filename1, n_arg, arg); task1(filename); break;
-            case 2: nameFile(&filename,&filename1, n_arg, arg); task2(filename); break;
-            case 3:
-            nameFile(&filename,&filename1, n_arg, arg);
-            task3(filename, filename1); break;
-            case 4:
-            nameFile(&filename, &filename1, n_arg, arg);
-            task4(filename, filename1); break;
+            case 1:  task1(filename); break;
+            case 2:  task2(filename); break;
+            case 3:task3(filename, filename1); break;
+            case 4:task4(filename, filename1); break;
             default: puts("Task number must be from 1 to 4!"); break;
         }
         puts("Do you want to try again?(y/n)");
